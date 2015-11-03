b9114b46e6787edc099ee319d79d1033cb55cfc4
aimee reiss & yvonne peng <student@epicodus.com>
2015-11-03 09:42:02 -0800

    Add initial files

---
+++ app/src/main/java/com/example/guest/congress/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.guest.congress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//
+//    https://congress.api.sunlightfoundation.com/legislators/locate?apikey=c7dff4303dd844eeaaeebdce9ca1fa8d&zip=
+// districts/locate?zip=11216
+//
+}
