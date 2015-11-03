31deeee3db6d0dce96867e000f428eae391e3114
aimee reiss & yvonne peng <student@epicodus.com>
2015-11-03 10:09:40 -0800

    Add LegislatorsActivity, get zip from user

--- app/src/main/java/com/example/guest/congress/MainActivity.java
+++ app/src/main/java/com/example/guest/congress/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.guest.congress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.zipcodeEditText) EditText mZipCodeEditText;
+    @Bind(R.id.searchButton) ImageButton mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LegislatorsActivity.class);
+                intent.putExtra("zipcode", mZipCodeEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
     }
-//
-//    https://congress.api.sunlightfoundation.com/legislators/locate?apikey=c7dff4303dd844eeaaeebdce9ca1fa8d&zip=
-// districts/locate?zip=11216
-//
+
 }
